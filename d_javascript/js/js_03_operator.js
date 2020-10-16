fa975185ddb0bf30cea6ca514d533d42d144c6e7
wjdgotn001 <wjdgotn001@gmail.com>
2020-10-16 17:57:20 +0900

    operator

---
+++ d_javascript/js/js_03_operator.js
@@ -0,0 +1,63 @@
+// js_03_operator.js
+
+console.log('start');
+
+var n = 10;
+//연산자(operator) : + - + / %
+//피연산자(operand) : 1 + 1에서 연산자를 제외한 부분
+/*
+var sum;
+sum = 10 + 20;
+console.log(sum);
+
+
+var min;
+min = 10 - 5;
+console.log(min);
+
+
+var multi;
+multi = n * 3;
+console.log(multi);
+
+var div;
+div = n / 3;
+divR = parseInt( div );
+console.log(div);
+
+
+var fac;
+fac = n % 3; // 10/3처리 시 몫을 제외한 나머지
+console.log(fac);
+
+console.log( 'n:', n );
+n = n + 2;
+console.log( n );
+
+var a = 2;
+a = a + 2;
+console.log( a );
+a = a + 10;
+console.log( a );
+a = a - 3;
+console.log( a );
+
+a += 5; // a = a+5;
+console.log( a );
+a -= 10; 
+console.log( a );
+a /= 2;
+console.log( a );
+a *= 100;
+console.log( a );
+*/
+
+var b = 0;
+b += 1;
+console.log(++b); //더하기(연속적으로)
+console.log( b );
+console.log(--b); //빼기(연속적으로)
+b++;
+console.log(b);
+
+
