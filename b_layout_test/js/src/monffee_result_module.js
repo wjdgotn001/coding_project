04a4d871d07eb3041a3f23bf17ef86ba70bdfa2b
wjdgotn001 <wjdgotn001@gmail.com>
2020-11-02 15:30:03 +0900

    html&js_connect

---
+++ b_layout_test/js/src/monffee_result_module.js
@@ -0,0 +1,15 @@
+
+// $(document).ready(function(){/* jQuery */});
+// $(function(){/* jQuery */});
+// (function($){})(jQuery);
+
+//jQuery에서는 선택자의 형태는 css와 매우 유사하다.
+//단, $(' ')를 이용하여 내부에 선택자를 작성한다.
+
+(function($){
+  //jQuery
+  
+  var headBox = $('#headBox');
+  headBox.load('./monffee/headBox.html');
+  
+})(jQuery);
