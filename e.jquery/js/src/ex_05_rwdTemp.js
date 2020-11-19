5225603f402179afa014603ebe09dda52a506745
wjdgotn001 <wjdgotn001@gmail.com>
2020-11-19 16:07:58 +0900

    반응형 연결2

--- e.jquery/js/src/ex_05_rwdTemp.js
+++ e.jquery/js/src/ex_05_rwdTemp.js
@@ -5,79 +5,88 @@
 3.브라우저의 크기가 변경될 때('resize') 기존 가로값과 변경된 가로값을 비교하여, 일치하지 않을 경우에 일부는 재처리
 */
 
-(function($){
-    
-    // var winW = win.css('width'); //문제점 1.문자로 값을 가져온다.(숫자로 변환해줘야함) 2.padding,margin,border의 값을 제외하고 가져온다.
-
-    // 선택자.width() -> 가로값을 수치로 계산
-    // .innerWidth() -> padding을 포함한 가로값
-    // .outerWidth() -> border(padding포함한)값을 포함한 가로값
-    // .outerWidth(true) -> margin(border,padding포함한)값을 포함한 가로값
+// var winW = win.css('width'); //문제점 1.문자로 값을 가져온다.(숫자로 변환해줘야함) 2.padding,margin,border의 값을 제외하고 가져온다.
+// 선택자.width() -> 가로값을 수치로 계산
+// .innerWidth() -> padding을 포함한 가로값
+// .outerWidth() -> border(padding포함한)값을 포함한 가로값
+// .outerWidth(true) -> margin(border,padding포함한)값을 포함한 가로값
 // =========================================================================
-    // 기준치를 설정
-    // [480, 1024, 1440, 1920];
+// 기준치를 설정
+// [480, 1024, 1440, 1920];
+
+// 반응형 파일 연결
+var deviceSize = function(){
+    var resultDevice; //최종적으로 만들어진 내용물에 대한 변수!!!!!!!!!(함수를 수행하기 전에 작성해야함)
+
+    (function($){
     
-    var deviceType = [
-        {type:'moblie', size:480},
-        {type:'tablet', size:1024},
-        {type:'labtop', size:1440},
-        {type:'pc', size:1920},
-        {type:'pcfull'}
-    ];
-
-    // 최초 가로값 파악
-    var win = $(window);
-    var beforeWinW = win.outerWidth(true);
-
-    var deviceCheck = function(winW){
-        // type체크
-    var myType;
-    /*
-    // if문==========================================
-    if(winW <= deviceType[0].size){
-        myType = deviceType[0].type;
-
-    }else if(winW <= deviceType[1].size){
-        myType = deviceType[1].type;
-
-    }else if(winW <= deviceType[2].size){
-        myType = deviceType[2].type;
-
-    }else if(winW <= deviceType[3].size){
-        myType = deviceType[3].type;
-
-    }else{
-        myType = deviceType[4].type;
-    }
-    */
-    // for문============================================
-    for(var i =0; i<deviceType.length ; i+=1 ){
-        if(winW <= deviceType[i].size){
-            myType = deviceType[i].type;
-            break;
+        
+        var deviceType = [
+            {type:'moblie', size:480},
+            {type:'tablet', size:1024},
+            {type:'labtop', size:1440},
+            {type:'pc', size:1920},
+            {type:'pcfull'}
+        ];
+    
+        // 최초 가로값 파악
+        var win = $(window);
+        var beforeWinW = win.outerWidth(true);
+    
+        var deviceCheck = function(winW){
+            // type체크
+        var myType;
+        /*
+        // if문==========================================
+        if(winW <= deviceType[0].size){
+            myType = deviceType[0].type;
+    
+        }else if(winW <= deviceType[1].size){
+            myType = deviceType[1].type;
+    
+        }else if(winW <= deviceType[2].size){
+            myType = deviceType[2].type;
+    
+        }else if(winW <= deviceType[3].size){
+            myType = deviceType[3].type;
+    
         }else{
             myType = deviceType[4].type;
         }
-    } //for
-    console.log(myType);
-    return myType;
-    }; //deviceCheck
+        */
+        // for문============================================
+        for(var i =0; i<deviceType.length ; i+=1 ){
+            if(winW <= deviceType[i].size){
+                myType = deviceType[i].type;
+                break;
+            }else{
+                myType = deviceType[4].type;
+            }
+        } //for
+        // console.log(myType);
+        return myType;
+        }; //deviceCheck
+    
+        var winSize = deviceCheck(beforeWinW);
+        resultDevice = winSize;
+    
+        // ========================================================
+        win.on('resize', function(){
+            
+            // 바뀐 가로값 파악
+            var reWinW = win.outerWidth(true);
+            var afterWinSize = deviceCheck(reWinW);
+    
+            // 기존 디바이스타입과 변경된 디바이스타입이 다른 경우에 새로고침
+            if(winSize !== afterWinSize){
+                location.reload();
+            }
+        });
+        return resultDevice; 
 
-
-    var winSize = deviceCheck(beforeWinW);
-
-
-    // ========================================================
-    win.on('resize', function(){
+    })(jQuery);
         
-        // 바뀐 가로값 파악
-        var reWinW = win.outerWidth(true);
-        var afterWinSize = deviceCheck(reWinW);
+    return resultDevice; //외부에서 확인가능
 
-        // 기존 디바이스타입과 변경된 디바이스타입이 다른 경우에 새로고침
-        if(winSize !== afterWinSize){
-            location.reload();
-        }
-    });
+} //deviceSize
 
-})(jQuery);
---
+++ e.jquery/js/src/ex_05_rwdTemp_import_module.js
@@ -0,0 +1,12 @@
+// ex_05_rwdTemp_import_module.js
+
+(function($){
+// jQuery start
+
+var nowW = deviceSize();
+console.log(nowW);
+
+
+
+
+})(jQuery);
